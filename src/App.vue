6ecaf77b684f541eaa9cded79dc44e93f1a535d4
Balazs Janoska <blage97@gmail.com>
2022-11-07 14:56:51 +0100

    Dragons with Vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,3 @@
-<!-- Start a new template and "clean it up" - remove stuff you dont need like in App.vue, delete HelloWorld.vue, delete the logo etc. -->
-<!-- Import dragons from src/assets/dragons.js (Data from: https://gist.github.com/tkfu/9819e4ac6d529e225e9fc58b358c3479 ) -->
 <!-- Create a childcomponent - DragonComp.vue -->
 <!-- Send dragondata from App.vue to DragonComp as a prop -->
 <!-- Build a simple dragon template in DragonComp where you show: Name, Skills, Challenge and display the img_url -->
@@ -13,28 +11,45 @@
 <!-- Bind the input to a value in props -->
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <DragonComp
+
+    @myCustomEvent="eventFromChild"
+
+     v-for="dragon in dragons" 
+     :key="dragon.name" 
+     :dragon="dragon"
+     ></DragonComp>
+  </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import dragons from '@/assets/dragons';
+import DragonComp from './components/DragonComp.vue';
 
 export default {
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      dragons,
+    };
+  },
+  components: {DragonComp},
+
+  methods: {
+    eventFromChild(oldName, newName){
+      this.dragons.forEach((dragon) => {
+        if(dragon.name === oldName){
+          dragon.name= newName;
+        }
+      })
+    },
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/DragonComp.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="dragons">
+        <h3>{{ dragon.name }}</h3>
+        <p>{{ dragon.Skills }}</p>
+        <p>{{ dragon.Challenge }}</p>
+        <img :src="dragon.img_url" alt="">
+
+        <form @submit.prevent="emitMethod">
+            <label for="betterName">Rename to:</label>
+            <input type="text" placeholder="Better name..." id="betterName"
+            v-model="newName"
+            required>
+            <input type="submit" value="Update name" />
+        </form>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DragonComp",
+    props: {
+        dragon: Object,
+    },
+    data(){
+        return{
+            newName: undefined
+        }
+    },
+
+    methods: {
+    emitMethod(){
+     this.$emit("myCustomEvent", this.dragon.name, this.newName);
+    },
+  },
+};
+
+</script>
+
+<style scoped>
+
+* {
+    margin: 0;
+    padding: 0;
+}
+
+h1,
+h2,
+h3,
+p,
+img {
+    margin-left: 5%;
+}
+
+h3 {
+    font-size: 2rem;
+}
+
+img {
+    width: 50%;
+}
+
+.dragons {
+    border: 3px solid black;
+    width: 40vw;
+    margin: 0 auto;
+    padding: 1rem;
+}
+</style>
